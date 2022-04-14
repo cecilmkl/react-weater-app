618a102e317dac4d2f92e54b4a2fc6acc7bf4337
Cecilie Klarpaas <cklarpaas@gmail.com>
2022-04-14 19:27:14 +0200

    Added link to GitHub

--- src/App.js
+++ src/App.js
@@ -7,6 +7,12 @@ function App() {
       <header className="App-header">
         <h1>Weather App</h1>
         <Search />
+        <footer>
+          <a href="https://github.com/cecilmkl/react-weater-app">
+            Open-source code
+          </a>{" "}
+          by Cecilie K.
+        </footer>
       </header>
     </div>
   );
